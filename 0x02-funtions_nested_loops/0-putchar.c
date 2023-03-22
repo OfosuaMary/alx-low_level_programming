04bbe150da13a32981942b8738b08920eb5142dd
OfosuaMary <maryas182@gmail.com>
2023-03-21 21:24:41 -0700

    _putchar

---
+++ 0x02-funtions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include "stdio.h"
+#include "main.h"
+/*function to print _putchar *\
+ int  output(char a)
+ {
+   putchar(a);
+   return 0;
+ }
+
+ int main()
+ {
+  int write = output("_putchar"); 
+
+  return 0;
+ }
---
+++ 0x02-funtions_nested_loops/main.h
@@ -0,0 +1,2 @@
+\* funtion to print out a character*\
+int output (char);
