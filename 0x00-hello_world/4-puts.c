8cada3a835b0f9fd679ee52c13d0c39557859464
OfosuaMary <maryas182@gmail.com>
2023-03-21 23:44:44 -0700

    Hello,puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include "stdio.h"
+/**
+ * main() : prints string of wods with put
+ * puts() : prints out string
+ *
+ * Return : 0
+ */
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle");
+	return (0);
+}
