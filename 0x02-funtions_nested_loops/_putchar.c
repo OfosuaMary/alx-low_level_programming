b0c3c4cd0fd0dd5be55252bb3ed0d5f2802f9473
OfosuaMary <maryas182@gmail.com>
2023-03-21 22:18:54 -0700

    _putchar corrections 1

---
+++ 0x02-funtions_nested_loops/_putchar.c
@@ -0,0 +1,15 @@
+#include "stdio.h"
+#include "main.h"
+/**
+ * _putchar: writes te character a to stdout
+ * @a: the character to print
+ *
+ * return: succes 1
+ * on error -1 is returned
+ */
+
+  int _putchar (char a)
+ {
+  
+   return (write(1, &a,1);
+ }
--- 0x02-funtions_nested_loops/main.h
+++ 0x02-funtions_nested_loops/main.h
@@ -1,2 +1,5 @@
+#ifndef main.h
+#define main.h
 \* funtion to print out a character*\
 int output (char);
+#endif
