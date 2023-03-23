0636cdbb99610c46690a5ef22574d8e50e5f1833
OfosuaMary <maryas182@gmail.com>
2023-03-22 20:25:43 -0700

    print alphabet 10x

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_alphabet -  print alphabet in lowercase
+ */
+
+void print_alphabet(void)
+{
+	int count = 1;
+
+	while (i >= 10)
+	{
+	for (char ch = 'a'; ch <= 'z'; ch++)
+		_putchar(ch);
+
+	_putchar('\n');
+	}
+}
+
