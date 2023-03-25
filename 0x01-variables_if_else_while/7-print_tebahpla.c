2c28737c7ac65ebb0717389fabc9411370ae4999
OfosuaMary <maryas182@gmail.com>
2023-03-25 07:21:21 -0700

    smile in the mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - print out lowercase alphabet
+ * Return: 0
+ */
+int main(void)
+{
+	char alph = 'z';
+
+	while (alph >= 'a')
+	{
+		putchar(alph);
+		alph--;
+	}
+	putchar('\n');
+	return (0);
+}
