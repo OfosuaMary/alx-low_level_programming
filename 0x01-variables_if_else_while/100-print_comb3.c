6fb56fa90d8e7c461d55f79098d08851f273bdc7
OfosuaMary <maryas182@gmail.com>
2023-03-25 11:45:34 -0700

    Inventing is a combination of brains and materials corrections2

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -7,21 +7,27 @@
 int main(void)
 {
 	int alph = '0';
-	int alph1;
+	long int alph1;
 
 	while (alph <= '8')
 	{
-		alph1 = '1';
+		alph1 = alph+1;
 		while (alph1 <= '9')
 		{
 			putchar(alph);
-			putchar(alph1);
+                        putchar(alph1);
 			if ((alph == '8') && (alph1 == '9'))
 			{
 				break;
 			}
+		/*	else if(alph == alph1)
+			{
+				continue;
+			}*/
 			else
 			{
+			/*	putchar(alph);*/
+			/*	putchar(alph1);*/
 				putchar(',');
 				putchar(' ');
 			}
