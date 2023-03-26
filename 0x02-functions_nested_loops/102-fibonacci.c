800c181ab5dc2661bbdc45fd7fe37b2a89ac4efe
OfosuaMary <maryas182@gmail.com>
2023-03-26 16:54:38 -0700

    fibonnaci

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,21 @@
+#include <stdio.>
+/**
+ * main - print 1st 50 fibonnaci start with 1 and 2
+ * Return: returns 0
+ */
+int main(void)
+{
+	int count, a, b, c;
+
+	a = 1;
+	b = 2;
+	printf("%d, %d", a, b);
+	for (count = 1; count <= 50; count++)
+	{
+		c = a + b;
+		printf(", %d", c);
+		a = b;
+		b = c;
+	}
+	return (0);
+}
