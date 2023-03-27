6ee81c91d97d7272f2d4b107073494c718c28ea1
OfosuaMary <maryas182@gmail.com>
2023-03-26 17:23:39 -0700

    Even liber Abbaci

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+/**
+ * main - print 1st 50 fibonnaci start with 1 and 2
+ * Return: returns 0
+ */
+int main(void)
+{
+	int count;
+	unsigned long sum, a, b, c;
+
+	a = 1;
+	b = 2;
+	c = 0;
+	sum = 0;
+	printf("%lu, %lu", a, b);
+	while (1)
+	{
+		if (c <= 4000000)
+		{
+		c = a + b;
+		if ((c % 2) == 0)
+		{
+			sum += c;
+		}
+		a = b;
+		b = c;
+		}
+	}
+	printf("%lu\n", sum);
+	return (0);
+}
