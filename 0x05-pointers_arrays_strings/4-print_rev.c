f86f557ee066a101cbc75e1c74eb6acbbb6a3708
OfosuaMary <maryas182@gmail.com>
2023-03-27 22:10:19 -0700

    reverse variable value of string corrections 2

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -12,6 +12,7 @@ void print_rev(char *s)
 	{
 	len++;
 	}
+	len -= 1;
 	while (len >= 0)
 	{
 		_putchar(s[len]);
--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stddef.h>
 /**
  * rev_string - prints string reverse
  * @s: String parameter
@@ -7,12 +8,13 @@
 void rev_string(char *s)
 {
 	int len, i = 0;
-	char rev[] = NULL;
+	char *rev = "";
 
 	while (s[len] != '\0')
 	{
 	len++;
 	}
+	len -= 1;
 	while (len >= 0)
 	{
 		rev[i] == s[len];
