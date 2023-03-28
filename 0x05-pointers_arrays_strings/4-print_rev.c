783f641eb325fdfcb43823f9c0b3546cc2efad4c
OfosuaMary <maryas182@gmail.com>
2023-03-27 21:20:53 -0700

    reverse of a string

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_rev - prints string reverse
+ * @s: String parameter
+ * Return: void
+ */
+void print_rev(char *s)
+{
+	int len, i = 0;
+
+	while (s[len] != '\0')
+	{
+	len++;
+	}
+	while (len >= 0)
+	{
+		_putchar(str[leni]);
+		len--;
+	}
+	_putchar('\n');
+}
