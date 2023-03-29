71abced711e03e0d9f353c6e58b4d75415cf1568
OfosuaMary <maryas182@gmail.com>
2023-03-28 19:43:27 -0700

    string copy 3

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,13 +1,15 @@
 #include "main.h"
+#include <string.h>
 /**
- * _strcpy -coppies using  pointer 
+ * _strcpy - coppies using  pointer
  * @dest: string pointer
- * #src: string pointer
+ * @src: string pointer
  * Return: pointer to a string
  */
 char *_strcpy(char *dest, char *src)
 {
 	int i;
+
 	for (i = 0; i < strlen(src); i++)
 	{
 		*(dest + i) = *(src + i);
