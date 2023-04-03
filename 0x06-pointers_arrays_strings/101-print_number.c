ca3658647e6c68c6d8826da1deb96c94fbbe03ca
ofosuaMary <maryas182@gmail.com>
2023-04-02 22:16:54 -0700

    prints an integer 1

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -3,35 +3,35 @@
  * print_number - prints integer
  * @n: integer parameter
  * Return: void
- * */
+ */
 void print_number(int n)
 {
-      if (n < 0)
-      {
-	_putchar('-');
-      	_putchar((-n / 10) + '0');
+	if (n < 0)
+	{
+		_putchar('-');
+	_putchar((-n / 10) + '0');
 	_putchar((-n % 10) + '0');
 	}
-      else if ((n >= 100) && (n < 1000))
-      {
-	      _putchar((n / 100) + '0');
-	      _putchar(((n / 10) % 10) + '0');
-	      _putchar((n % 10) + '0');
-      }
-      else if (n >= 1000)
-      {
-      	_putchar((n / 1000) + '0');
+	else if ((n >= 100) && (n < 1000))
+	{
+	_putchar((n / 100) + '0');
+	_putchar(((n / 10) % 10) + '0');
+	_putchar((n % 10) + '0');
+	}
+	else if (n >= 1000)
+	{
+	_putchar((n / 1000) + '0');
 	_putchar(((n / 100) % 10) + '0');
 	_putchar(((n / 10) % 10) + '0');
 	_putchar((n % 10) + '0');
-      }
-       else if (n == 0)
-      {
-        _putchar((n) + '0');
-      }
-      else
-      {
-      	_putchar((n / 10) + '0');
+	}
+	else if (n == 0)
+	{
+	_putchar((n) + '0');
+	}
+	else
+	{
+	_putchar((n / 10) + '0');
 	_putchar((n % 10) + '0');
-      }
+	}
 }
