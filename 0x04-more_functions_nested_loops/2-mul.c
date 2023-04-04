bed6da5cfcffc5d3aec2d3c66d5efb5352f8cf6b
ofosuaMary <maryas182@gmail.com>
2023-04-04 16:18:19 -0700

    multipliction

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * mul - multiplies 2 intergers
+ * @a: interger parameter
+ * @b: integer parameter
+ * Return: results after multiplication
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
